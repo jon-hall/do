f63dc0d5f090ab91e2d210007c454f3d45ed7aa7
Jon Hall <jon_hall@outlook.com>
2016-04-04 21:54:59 +0100

    better mock runner

--- test/utils/mock-runner.js
+++ test/utils/mock-runner.js
@@ -1,21 +1,20 @@
 import RunnerBase from '../../src/runner-base.js';
 
 export default class MockRunner extends RunnerBase {
-    async run(prompts, state) {
+    async run(prompts, state = {}) {
         this.prompts = Array.isArray(prompts) ? prompts : [prompts];
-        this.current = this.prompts.shift();
-        this.state = state || {};
-        this.waiter = new Promise(resolve => this.resolve = resolve);
-        await this.waiter;
+        this.state = state;
+        return new Promise(resolve => this.resolve = resolve);
     }
 
     respond(value) {
-        this.state[this.current.name] = value;
+        // Respond to next prompt by setting state
+        const responded = this.prompts.shift();
+        this.state[responded.name] = value;
 
+        // And resolve when no more prompts to respond to
         if(!this.prompts.length) {
             return this.resolve(this.state);
         }
-
-        this.current = this.prompts.shift();
     }
 }
